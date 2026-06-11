9b12aeb28b1555c391f16067e616af788793b576
WOHO99 <WOHO99@users.noreply.github.com>
2026-06-11 21:17:57 +0800

    P4-6: fix parse_published_time for +0000 format without colon (NHK World RSS)

--- scripts/common.py
+++ scripts/common.py
@@ -257,7 +257,7 @@ def parse_published_time(pub_str):
     if not pub_str:
         return None
     for fmt in [
-        "%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%dT%H:%M:%SZ",
+        "%Y-%m-%dT%H:%M:%S.%f%z", "%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%dT%H:%M:%SZ",
         "%a, %d %b %Y %H:%M:%S %z", "%a, %d %b %Y %H:%M:%S GMT", "%Y-%m-%d",
     ]:
         try:
